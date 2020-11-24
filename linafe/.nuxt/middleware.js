decce88e2cc344847a3ff3cf8197e0acd5c3ad0c
Galvanino <galvanino@gmail.com>
2020-11-24 17:11:05 -0500

    Borrando linafe/.nuxt de GitHub

--- linafe/.nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['auth_lina'] = require('../middleware/auth_lina.js')
-middleware['auth_lina'] = middleware['auth_lina'].default || middleware['auth_lina']
-
-middleware['guest'] = require('../middleware/guest.js')
-middleware['guest'] = middleware['guest'].default || middleware['guest']
-
-export default middleware
