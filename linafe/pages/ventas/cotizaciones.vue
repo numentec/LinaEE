b219867842e61f9f4af26d40b691cbda48ee7b1b
Galvanino <galvanino@gmail.com>
2020-12-08 21:08:34 -0500

    01 - Se instaló DevExtreme v20.2 para VueJS 02 - Se implementó el tema dx.material.linaee_cerceta.css 03 - Se desarrolló un grid de prueba en la página cotizaciones 04 - Se creó un servicio linatest en docker-compose-prod.yml para probar el deployd con componentes DevExtreme 05 - Se cambió el puerto de escucha del server en producción de 3001 a 30059

--- linafe/pages/ventas/cotizaciones.vue
+++ linafe/pages/ventas/cotizaciones.vue
@@ -1,29 +1,132 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/uc2.jpg" alt="***** En Construcción *****" class="mb-5" />
-      <blockquote class="blockquote">
-        <h5>{{ page_name }}</h5>
-        &#8220;En desarrollo por el equipo de NumenTec.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;www.numentec.com</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
-</template>
+  <MaterialCard class="mt-10">
+    <template v-slot:heading>
+      <v-toolbar dense color="secondary" dark flat>
+        <v-btn dark icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+        <v-spacer />
+        <v-toolbar-title>Demo Data</v-toolbar-title>
+        <v-spacer />
+        <v-btn dark icon @click="showColumnChooser">
+          <v-icon>mdi-table-column-plus-after</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </template>
+    <DxDataGrid
+      :ref="curGridRefKey"
+      class="ma-4"
+      :data-source="dataSource"
+      :remote-operations="false"
+      :allow-column-reordering="true"
+      :row-alternation-enabled="true"
+      :show-borders="true"
+      @content-ready="onContentReady"
+    >
+      <DxColumn :group-index="0" data-field="Product" />
+      <DxColumn
+        data-field="Amount"
+        caption="Sale Amount"
+        data-type="number"
+        format="currency"
+        alignment="right"
+      />
+      <DxColumn
+        :allow-grouping="false"
+        data-field="Discount"
+        caption="Discount %"
+        data-type="number"
+        format="percent"
+        alignment="right"
+        cell-template="discountCellTemplate"
+        css-class="bullet"
+      />
+      <DxColumn data-field="SaleDate" data-type="date" />
+      <DxColumn data-field="Region" data-type="string" />
+      <DxColumn data-field="Sector" data-type="string" />
+      <DxColumn data-field="Channel" data-type="string" />
+      <DxColumn :width="150" data-field="Customer" data-type="string" />
 
+      <DxGroupPanel :visible="true" />
+      <DxSearchPanel :visible="true" :highlight-case-sensitive="true" />
+      <DxColumnChooser mode="select" />
+      <DxGrouping :auto-expand-all="false" />
+      <DxPager
+        :allowed-page-sizes="pageSizes"
+        :show-page-size-selector="true"
+      />
+      <DxPaging :page-size="10" />
+      <template #discountCellTemplate="{ data: cellData }">
+        <DiscountCell :cell-data="cellData" />
+      </template>
+    </DxDataGrid>
+  </MaterialCard>
+</template>
 <script>
+import {
+  DxDataGrid,
+  DxColumn,
+  DxGrouping,
+  DxGroupPanel,
+  DxPager,
+  DxPaging,
+  DxSearchPanel,
+  DxColumnChooser,
+} from 'devextreme-vue/data-grid'
+
+import DataSource from 'devextreme/data/data_source'
+import 'devextreme/data/odata/store'
+
+import DiscountCell from '~/components/core/DiscountCell.vue'
+
+const curGridRefKey = 'my-cur-grid'
+
+let collapsed = false
+
 export default {
+  components: {
+    DxDataGrid,
+    DxColumn,
+    DxGrouping,
+    DxGroupPanel,
+    DxPager,
+    DxPaging,
+    DxSearchPanel,
+    DiscountCell,
+    DxColumnChooser,
+    MaterialCard: () => import('~/components/core/MaterialCard'),
+  },
   data() {
     return {
-      page_name: 'page',
+      curGridRefKey,
+      dataSource: new DataSource({
+        store: {
+          type: 'odata',
+          url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
+          beforeSend(request) {
+            request.params.startDate = '2018-05-10'
+            request.params.endDate = '2018-05-15'
+          },
+        },
+      }),
+      pageSizes: [10, 25, 50, 100],
+      onContentReady(e) {
+        if (!collapsed) {
+          e.component.expandRow(['EnviroCare'])
+          collapsed = true
+        }
+      },
     }
   },
-  created() {
-    this.page_name = this.$nuxt.$route.name.toUpperCase()
+  computed: {
+    curGrid() {
+      return this.$refs[curGridRefKey].instance
+    },
+  },
+  methods: {
+    showColumnChooser() {
+      this.curGrid.showColumnChooser()
+    },
   },
 }
 </script>
