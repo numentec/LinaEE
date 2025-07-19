2160d1b7768a845244b39656aadbb7661cae9082
numentec <numentec@gmail.com>
2025-07-19 15:51:29 -0500

    Se agregaron comentarios explicativos

--- linabe/apps/catalog/urls.py
+++ linabe/apps/catalog/urls.py
@@ -17,12 +17,15 @@ router = DefaultRouter()
 
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'tags', TagViewSet, basename='tags')
-router.register(r'catalogs', CatalogViewSet, basename='catalogs')
-router.register(r'catalogsonly', CatalogMasterViewSet, basename='catalogsonly')
-router.register(r'catalog-details', CatalogDetailViewSet, basename='catalog-details')
-router.register(r'catalog-detail-images', CatalogDetailImageViewSet, basename='catalog-detail-images')
+router.register(r'catalogs', CatalogViewSet, basename='catalogs') # Devuelve Master y detalles del catálogo
+router.register(r'catalogsonly', CatalogMasterViewSet, basename='catalogsonly') # Devuelve solo el maestro del catálogo
+router.register(r'catalog-details', CatalogDetailViewSet, basename='catalog-details') # Devuelve detalles del catálogo
+router.register(r'catalog-detail-images', CatalogDetailImageViewSet, basename='catalog-detail-images') # Devuelve imágenes de detalles del catálogo
 
 urlpatterns = [
+    # Devuelve los catálogos activos de un cliente
+    # Se usa el ulid del cliente para filtrar los catálogos activos
+    # y el token para validar el acceso a un catálogo específico
     path('by-customer/<str:ulid>/', ActiveCustomerCatalogsAPIView.as_view(), name='active-customer-catalogs'),
     path('by-customer/<str:ulid>/<str:token>/', ValidCustomerCatalogAPIView.as_view(), name='valid-customer-catalog'),
     path('', include(router.urls)),
