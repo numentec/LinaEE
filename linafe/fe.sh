c78ad6ece348ea09bf177b06fe061c18a541a8e0
numentec <numentec@gmail.com>
2025-11-12 18:34:02 -0500

    Recrea scripts y agrega .gitattributes para corregir incidencias con salto de línea

---
+++ linafe/fe.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -e
+
+: "${HOST:=0.0.0.0}"
+: "${PORT:=3001}"
+
+export HOST PORT
+
+if [ "$NODE_ENV" = "production" ]; then
+  # To build and start the application in production mode, uncomment the lines below:
+  echo "Starting in production on $HOST:$PORT"
+  # npm run build
+  npm run start
+else
+  # Start the application in development mode
+  echo "Starting in dev on $HOST:$PORT"
+  npm run dev
+fi
+
