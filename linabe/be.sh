67eb08f0fba18951d3e6b518d4445e5da01e0f6d
numentec <numentec@gmail.com>
2026-01-30 19:09:46 -0500

    Cambia arranque del proyecto a gunicorn con 2 workers. Se hizo por funcionalidad de exportar a PDF

--- linabe/be.sh
+++ linabe/be.sh
@@ -2,10 +2,18 @@
 
 set -e
 
-python ./manage.py wait_for_db
+# python ./manage.py wait_for_db
 
 # python ./manage.py collectstatic --noinput
 # python ./manage.py migrate --fake
 
 # exec gunicorn linapi.wsgi --bind 0.0.0.0:8001
-exec python ./manage.py runserver 0.0.0.0:8001
+
+exec gunicorn linapi.wsgi:application \
+  --bind 0.0.0.0:8001 \
+  --workers 2 \
+  --threads 4 \
+  --timeout 180
+
+
+# exec python ./manage.py runserver 0.0.0.0:8001
