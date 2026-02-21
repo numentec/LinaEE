1b201b40a615045d6dbd159a41866b6a874d5dc3
numentec <numentec@gmail.com>
2026-02-21 17:54:29 -0500

    Agrega valor predeterminado para NODE_ENV en fe.sh. Aplica modificaciones considerando valor default de NODE_ENV

--- linafe/fe.sh
+++ linafe/fe.sh
@@ -4,17 +4,18 @@ set -e
 
 : "${HOST:=0.0.0.0}"
 : "${PORT:=3001}"
+: "${NODE_ENV:=development}"
 
-export HOST PORT
+export HOST PORT NODE_ENV
+
+echo "Starting in $NODE_ENV on $HOST:$PORT"
 
 if [ "$NODE_ENV" = "production" ]; then
-  # To build and start the application in production mode, uncomment the lines below:
-  echo "Starting in production on $HOST:$PORT"
+  # To build the application in production mode, uncomment the line below:
   # npm run build
   npm run start
 else
   # Start the application in development mode
-  echo "Starting in dev on $HOST:$PORT"
   npm run dev
 fi
 
