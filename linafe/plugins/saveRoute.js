973ba5fa12b90980b011bb2a93e6c02fbf245783
numentec <numentec@gmail.com>
2026-03-03 21:58:41 -0500

    Agrega validación para no añadir path /login en saveRoute.js

--- linafe/plugins/saveRoute.js
+++ linafe/plugins/saveRoute.js
@@ -1,7 +1,17 @@
 export default ({ app }) => {
   app.router.beforeEach((to, from, next) => {
     if (process.client) {
-      localStorage.setItem('lina_lastRoute', to.fullPath)
+      const isToLogin = to.path && to.path.startsWith('/login')
+      const isFromLogin = from.path && from.path.startsWith('/login')
+
+      if (!isFromLogin && from.path) {
+        localStorage.setItem('lina_prevPath', from.path)
+      }
+
+      if (!isToLogin && to.fullPath) {
+        // localStorage.setItem('lina_lastRoute', to.fullPath)
+        localStorage.setItem('lina_lastPath', to.fullPath)
+      }
     }
     next()
   })
